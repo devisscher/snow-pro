7a259dae9aa1281c0320b59aa3a87080a4bf588c
devisscher <devisscher.thomas@gmail.com>
2020-08-08 12:49:48 -0400

    get stuf running

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Geolocation from 'react-native-geolocation-service';
-import gql from 'graphql-tag';
-import { useQuery } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
-
-import { ApolloProvider } from '@apollo/react-hooks';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Geolocation from "react-native-geolocation-service";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import { Button } from "@material-ui/core";
+import { ApolloProvider } from "@apollo/react-hooks";
+import AppNavigation from "./components/AppNavigation";
+import 'react-native-gesture-handler';
+interface LocationData {
+  latitude: number;
+  longitude: number;
+}
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8084',
+  uri: "http://localhost:8084",
 });
 
 const GET_WEATHER = gql`
@@ -26,59 +32,56 @@ const GET_WEATHER = gql`
 `;
 
 const getWeather = (latitude, longitude) => {
-  console.log('calling gql endpont with: ', latitude, longitude)
+  console.log("calling gql endpont with: ", latitude, longitude);
   const { loading, error, data } = useQuery(GET_WEATHER);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
-  return (<Text>
-    {JSON.stringify(data)}
-  </Text>);
-}
+  return <Text>{JSON.stringify(data)}</Text>;
+};
 
 export default function App() {
-  const [location, setLocation] = useState()
+  const [location, setLocation] = useState<LocationData | undefined>();
 
-
-  const getLocation = () => Geolocation.getCurrentPosition(
-    (position) => {
-      console.log(position);
-      setLocation(position.coords)
-      getWeather(location.latitude, location.longitude)
-      console.log('location: ', location)
-    },
-    (error) => {
-      // See error code charts below.
-      console.log(error.code, error.message);
-    },
-    { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-  );
-
-
-
+  const getLocation = () =>
+    Geolocation.getCurrentPosition(
+      (position) => {
+        console.log("position", position);
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
+      (error) => {
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
 
   useEffect(() => {
-    console.log('getting location')
-    getLocation()
-  }, [])
+    console.log("getting location");
+    getLocation();
+  }, []);
   return (
-    <ApolloProvider client={client}>
-      <View style={styles.container}>
-        <Text>You are here</Text>
-        <Text>
-          {`${location.latitude} & ${location.longitude}`}
-        </Text>
-      </View>
-    </ApolloProvider>
+      <ApolloProvider client={client}>
+        <View style={styles.container}>
+          <Text>You are here</Text>
+          <Text>
+            {`${location?.latitude || "N/A"} & ${location?.longitude || "N/A"}`}
+          </Text>
+          <Button color="primary">Hello World</Button>
+          <AppNavigation />
+        </View>
+      </ApolloProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
